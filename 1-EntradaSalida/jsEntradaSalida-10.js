2443e6831611fa60663c6198fb96c866ee59f7cb
XaeroreaX <martinpitameglia@gmail.com>
2017-02-05 14:16:58 -0300

    terminamos los ejercicios de entrada y salida
    en esta tanda de ejercicios aprendimos a mostrar un alert por pantalla, pedir y tomar datos con promt, tomar datos de un id con document.getElementById y pasar a enteros con un ParseInt

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	//id importe
+	var importe = document.getElementById("importe").value;
+	importe = parseInt(importe) - ((parseInt(importe)*25)/100); // aca usamos la regla de 3 para estar seguros
+	alert(importe);
 }
