3f9533a940814e6cb59cb6a22d19b562a51dd20f
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 11:08:11 -0300

    ejercicios extraño terminado

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
-
+	var edad = document.getElementById("edad").value;
+	var ECivil = document.getElementById("estadoCivil").value;
+	if(parseInt(edad) >= 18 && ECivil == "Soltero")
+		alert("Es soltero y no es menor.")
 	
 
 
