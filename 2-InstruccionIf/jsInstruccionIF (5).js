dd3f9a9cd5bf02ce751fe1b9074cca2e276d9b33
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 10:17:22 -0300

    ojo con las condiciones logicas
    or es ||

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,17 @@
+/*Al ingresar una edad solo debemos informar si la persona NO es adolescente.*/
 function Mostrar()
 {
 //tomo la edad  
-
-
+	var Edad = document.getElementById("edad").value;
+	Edad = parseInt(Edad);
+	if(Edad <= 0)
+	{
+		alert("AVISO:ingrese bien la edad!");
+	}
+	else
+	{    
+		if(Edad < 13 || Edad > 17) // me confundi con el or
+			alert("no es adolescente");
+		
+	}
 }//FIN DE LA FUNCIÓN
