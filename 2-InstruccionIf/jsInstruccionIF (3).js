b9cd8c4b415006ef48b55a22c5d1b70ddd37a2bc
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 09:38:37 -0300

    aprendiendo if: mayor de edad
    no pongan 0<=

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+var Edad = document.getElementById("edad").value;
 
+	if(parseInt(Edad)<=0)
+	{
+		alert("AVISO:ingrese bien la edad!")
+	}
+	else
+	{
+		if(parseInt(Edad)>=18)
+		{
+			alert("es mayor de edad");
+		} // por vagancia copiamos y pegamos
+		else
+		{
+			alert("es menor de edad");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
