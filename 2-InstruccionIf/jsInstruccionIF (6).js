2cef6b3b6bf9c452bb5edb60e9c2fbb4d4dc0f2f
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 10:31:43 -0300

    aprendiendo if: ejercicio terminado

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,30 @@ function Mostrar()
 {
 //tomo la edad  
 
-
-
-
+	var Edad = document.getElementById("edad").value;
+	Edad = parseInt(Edad);
+	if(Edad <= 0)//bloqueamos edades incoherentes
+	{
+		alert("AVISO:ingrese bien la edad!");
+	}
+	else
+	{    
+		if(Edad>=18)//preguntamos si es mayor de edad
+		{
+			alert("es mayor de edad");
+		}
+		else
+		{
+			if(Edad >= 13)//preguntamos si es adolecente
+			{
+				alert("es adolescente");
+			}
+			else//aca los ponemos como niño por defecto
+			{
+				alert("es un niño");
+			}
+		}
+		
+	}
+ 
 }//FIN DE LA FUNCIÓN
