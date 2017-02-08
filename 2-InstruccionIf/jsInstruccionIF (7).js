138c2285104a1cd4fd66b1e99fe7c0ca9a379fd1
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 10:54:13 -0300

    ejercicio terminado
    aprendimos sobre el select id

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,9 @@
 function Mostrar()
 {
 //tomo la edad  
-
-	
-
-
+// Id: edad y estadoCivil
+	var edad = document.getElementById("edad").value;
+	var ECivil = document.getElementById("estadoCivil").value;
+	if(parseInt(edad) <= 18 && ECivil != "Soltero")
+		alert("Es muy pequeño para NO ser soltero.")
 }//FIN DE LA FUNCIÓN
