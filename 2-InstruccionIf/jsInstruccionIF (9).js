66e89f40bf9a25570049ad699ea6b94c6efdf52c
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 11:51:20 -0300

    numero aleatorio
    usamos Math.random()

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	//id: FormIngreso 
+	//generacion de numeros random Math.random() * numPosibilidades 
+	var NunRandom;
+	NunRandom = parseInt(NunRandom);
+	NunRandom = parseInt(Math.random() * 10)+1;
+	alert(NunRandom);
+	document.getElementById("FormIngreso").value = NunRandom;
 
 }//FIN DE LA FUNCIÓN
