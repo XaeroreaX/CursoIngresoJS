50fcab1a7ef5bd1977ef553d0c3426c48e135bb8
XaeroreaX <martinpitameglia@gmail.com>
2017-02-08 09:58:23 -0300

    aprendiendo if anidados: ejercicios terminados
    if anidados

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,21 @@
 function Mostrar()
-{
-//tomo la edad  
+{//tomo la edad
+	  
 
-
-
+	var Edad = document.getElementById("edad").value;
+	if(parseInt(Edad)<=0)
+		{
+			alert("AVISO:ingrese bien la edad!");
+		}
+		else
+		{
+			if(parseInt(Edad)>=13 && parseInt(Edad)<=17)
+			{	
+				alert("es adolescente");
+			}
+			else
+			{
+				alert("no es adolescente");
+			}	
+		}
 }//FIN DE LA FUNCIÓN
