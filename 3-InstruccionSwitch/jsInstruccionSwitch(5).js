e3cee8e09fc2296c0242b2d3370ab31bc3ea237a
XaeroreaX <martinpitameglia@gmail.com>
2017-02-09 10:39:05 -0300

    ejercicio terminado
    copie y pegue la variable del ejercicio anterior jaja

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,3 +1,6 @@
+/*
+Al ingresar una hora, informar: 
+si está entre las 7 y las 11 : "Es de mañana.".*/
 function Mostrar()
 {
 //tomo la edad  
@@ -5,7 +8,30 @@ var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
 	
-	
+	switch(parseInt(laHora)) //hay que pasarlo a entero como el if
+	{
+		case 7:
+	      	alert("Es de mañana.");
+	    break;
+
+		case 8:
+	      	alert("Es de mañana.");
+	    break;
+
+		case 9:
+	      	alert("Es de mañana.");
+	    break;
+
+		case 10:
+	      	alert("Es de mañana.");
+	    break;
+
+		case 11:
+	      	alert("Es de mañana.");
+	    break;
+	    default:
+	    	alert("no es mañana.");
+	}	    
 
 
 
