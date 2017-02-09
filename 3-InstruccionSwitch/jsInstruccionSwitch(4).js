150d27a16a0aa21623186be4cacc27d372a5410d
XaeroreaX <martinpitameglia@gmail.com>
2017-02-09 10:17:24 -0300

    ejercicio terminado
    ahora podre saber cuantos dias tiene un mes

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,54 @@
+/*al seleccionar un mes informar. 
+si tiene 28 días. 
+si tiene 30 días. 
+si tiene 31 días.*/
+
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+	//alert (mesDelAño);
+
+	switch(mesDelAño) //un copypaste del ejercicio anterior
+	{
+		case "Enero":
+	      	alert("tiene 31 días.");
+	    break;
+	    case "Febrero":
+	      	alert("tiene 28 días");
+	    break;
+	    case "Mayo":
+	      	alert("tiene 31 días.");
+	    break;
+	    case "Abril":
+	      	alert("tiene 30 días.");
+	    break;
+	    case "Marzo":
+	      	alert("tiene 31");
+	    break;
+	    case "Junio":
+	      	alert("tiene 30 días.");
+	    break;
+	   	case "Julio":
+	      	alert("tiene 31 días.");
+	    break;
+	    case "Agosto":
+	      	alert("tiene 31 días.");
+	    break;
+	    case "Septiembre":
+	      	alert("tiene 30 días.");
+	    break;
+	    case "Octubre":
+	      	alert("tiene 31 días.");
+	    break;
+	    case "Noviembre":
+	      	alert("tiene 30 días.");
+	    break;
+	    case "Diciembre":
+	      	alert("tiene 31 días.");
+	    break;
+	}		
 
 
 
