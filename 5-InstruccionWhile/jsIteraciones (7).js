4ee5a018912402b2003c3bf06b60acce5883ed57
XaeroreaX <martinpitameglia@gmail.com>
2017-02-15 11:56:02 -0300

    while,terminado

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,18 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
 
+
+	while(respuesta == 'si' || respuesta == 'SI')
+	{
+		numero = prompt("ingrese el numero");
+		numero = parseInt(numero);
+		acumulador = acumulador + numero;
+		contador++;
+		respuesta = prompt("quiere seguir ingresando?(en minuscual)");
+
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
