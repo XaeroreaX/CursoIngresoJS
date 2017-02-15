6d4e675d9a3959bb815a3116b5184dc0d2bcdc54
XaeroreaX <martinpitameglia@gmail.com>
2017-02-15 12:07:18 -0300

    while, terminado y con confirm

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,18 +3,17 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta= true;//los valores de confirm
 	var numero;
 
-
-	while(respuesta == 'si' || respuesta == 'SI')
+	respuesta = confirm("quiere seguir ingresando?aceptar/cancelar");
+	while(respuesta == true)
 	{
 		numero = prompt("ingrese el numero");
 		numero = parseInt(numero);
 		acumulador = acumulador + numero;
 		contador++;
-		respuesta = prompt("quiere seguir ingresando?(en minuscual)");
-
+		respuesta = confirm("quiere seguir ingresando?aceptar/cancelar"); 
 	}
 
 document.getElementById('suma').value=acumulador;
