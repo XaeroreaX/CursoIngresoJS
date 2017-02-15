57ea12ea87a3c484b08a1d88fd8d3d4f200f1f4a
XaeroreaX <martinpitameglia@gmail.com>
2017-02-15 11:44:18 -0300

    while, terminado, erro en parsear
    el PROMPT SOLO TOMA TEXTO!!!

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,18 @@
 function Mostrar()
 {
-
-	var contador=0;
-	var acumulador=0;
-
-
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	var contador=parseInt(contador);
+	contador=1;
+	var acumulador=parseInt(acumulador);
+	acumulador=0;
+	var numero;
+	while(contador<=5)
+	{
+		numero = prompt("ingrese el numero ");//el prompt toma solo texto!!!
+		numero = parseInt(numero);
+		acumulador = acumulador + numero;
+		contador++;
+	}
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
