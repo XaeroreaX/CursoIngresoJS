9b5cef298a63d016a0eb7af2e864e92697050cb4
XaeroreaX <martinpitameglia@gmail.com>
2017-02-16 09:40:00 -0300

    while, terminado
    lea bien las consignas y practicar lectura comprensiva

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,45 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
-
-	while(respuesta!='no')
-	{
 		
-	
+	var contador=1; //declaramos variables
+	var menor;
+	var mayor;
+	var numero = 0;
+	var respuesta ;
+	respuesta = confirm("quiere ingresar un numero?");//entrar con confirm
+	if(respuesta = true)
+	{
+		numero=prompt("ingrese el numero "+contador);
+		numero = parseInt(numero);
+		mayor = numero;
+		menor = numero;
+		respuesta = confirm("quiere ingresar otro numero?");
+		while(respuesta == true)
+		{
+			contador++
+			numero = prompt("ingrese el numero "+contador);
+			numero = parseInt(numero);
+		   
+		
+			if(numero < menor) //acumular los negativos y lo positivos por separado
+			{
+				menor = numero;
+			}
+			else
+			{
+				if(numero>mayor)
+					mayor = numero;
+			}
+		
+
+
+			respuesta = confirm ("quiere ingresar otro numero?");
+		
+		}
 	}
-
-
+	document.getElementById('maximo').value = mayor;
+	document.getElementById('minimo').value = menor;
 
 
 }//FIN DE LA FUNCIÓN
