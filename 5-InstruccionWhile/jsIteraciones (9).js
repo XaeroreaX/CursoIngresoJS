9892c8d8195a767d0b0f357a586e7903f8c6aeb0
XaeroreaX <martinpitameglia@gmail.com>
2017-02-16 09:44:44 -0300

    while, terminado
    y bien comentado

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -7,22 +7,30 @@ function Mostrar()
 	var mayor;
 	var numero = 0;
 	var respuesta ;
-	respuesta = confirm("quiere ingresar un numero?");//entrar con confirm
+
+
+
+	respuesta = confirm("quiere ingresar un numero?");//entrar con confirmar si quiere ingresar un numero
 	if(respuesta = true)
 	{
 		numero=prompt("ingrese el numero "+contador);
 		numero = parseInt(numero);
+		//inicializar los contadores
 		mayor = numero;
 		menor = numero;
-		respuesta = confirm("quiere ingresar otro numero?");
+
+
+		respuesta = confirm("quiere ingresar otro numero?");//mientraz mas opciones mejor
 		while(respuesta == true)
 		{
+
 			contador++
+
 			numero = prompt("ingrese el numero "+contador);
 			numero = parseInt(numero);
 		   
 		
-			if(numero < menor) //acumular los negativos y lo positivos por separado
+			if(numero < menor) //aca sacamos los mayores y los minimos de cada ingreso de numero
 			{
 				menor = numero;
 			}
@@ -34,11 +42,13 @@ function Mostrar()
 		
 
 
-			respuesta = confirm ("quiere ingresar otro numero?");
+			respuesta = confirm ("quiere ingresar otro numero?");//confirm para salir del while
 		
 		}
 	}
-	document.getElementById('maximo').value = mayor;
+
+
+	document.getElementById('maximo').value = mayor;// mostra por pantalla
 	document.getElementById('minimo').value = menor;
 
 
