f4e683944d686ef16237e92f6a71de59d80d7e15
XaeroreaX <martinpitameglia@gmail.com>
2017-02-16 08:59:45 -0300

    while, terminado
    muy parecido al anterior y salir con confirm

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,34 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador=1; //declaramos variables
 	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
+	var negativo=0;
+	var numero = 0;
+	var respuesta;
+	respuesta = confirm("quiere ingresar un numero?");//entrar con confirm
+	while(respuesta == true)
+	{
+		numero=prompt("ingrese el numero "+contador);
+		numero = parseInt(numero);
+		if(numero != 0)   //escluir el 0
+		{
+			if(numero < 0) //acumular los negativos y lo positivos por separado
+			{
+				negativo = negativo + numero;
+			}
+			else
+			{
+				positivo = positivo + numero;
+			}
+		}
 
+
+		respuesta = confirm ("quiere ingresar otro numero?");
+		contador++
+	}
 
 document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+document.getElementById('producto').value=negativo; //mostrar por pantalla
 
 }//FIN DE LA FUNCIÓN
