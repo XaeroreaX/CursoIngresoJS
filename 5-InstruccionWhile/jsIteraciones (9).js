3fb6fec033a7dd579da2324950aa3b603796558a
XaeroreaX <martinpitameglia@gmail.com>
2017-02-16 11:31:26 -0300

    while, terminado
    una mejor forma de inicializar los maximos y minimos

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -8,44 +8,43 @@ function Mostrar()
 	var numero = 0;
 	var respuesta ;
 
-
-
-	respuesta = confirm("quiere ingresar un numero?");//entrar con confirmar si quiere ingresar un numero
-	if(respuesta = true)
+	
+	respuesta = confirm("quiere ingresar otro numero?");//mientraz mas opciones mejor
+	while(respuesta == true)
 	{
-		numero=prompt("ingrese el numero "+contador);
+
+		
+
+		numero = prompt("ingrese el numero "+contador);
 		numero = parseInt(numero);
-		//inicializar los contadores
-		mayor = numero;
-		menor = numero;
-
-
-		respuesta = confirm("quiere ingresar otro numero?");//mientraz mas opciones mejor
-		while(respuesta == true)
+		if(contador==1)// la mejor forma objetibamente hablando de inicializar los maximos y minimos 
+		{
+			mayor = numero;
+			menor = numero;
+		}
+		else
 		{
 
-			contador++
-
-			numero = prompt("ingrese el numero "+contador);
-			numero = parseInt(numero);
-		   
-		
 			if(numero < menor) //aca sacamos los mayores y los minimos de cada ingreso de numero
 			{
 				menor = numero;
 			}
 			else
 			{
-				if(numero>mayor)
-					mayor = numero;
-			}
-		
+			if(numero>mayor)
+				mayor = numero;
+			}//if(numero < menor)
 
 
-			respuesta = confirm ("quiere ingresar otro numero?");//confirm para salir del while
+		}//if(contador==1)
 		
-		}
-	}
+		
+
+		contador++;
+		respuesta = confirm ("quiere ingresar otro numero?");//confirm para salir del while
+		
+	}//while(respuesta == true)
+	
 
 
 	document.getElementById('maximo').value = mayor;// mostra por pantalla
