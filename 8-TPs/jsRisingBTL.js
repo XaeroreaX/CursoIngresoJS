b8c5be3b77210adc26883ac3e4b70fa2b231b4c2
XaeroreaX <martinpitameglia@gmail.com>
2017-02-19 22:59:36 -0300

    tp, terminado
    con funcion de que elegir

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -15,12 +15,69 @@ function ComenzarIngreso ()
 	var edad;
 	var sex;
 	var estadoCivil;
-	var validarSwitch = true;
 	var sueldo;
 	var legajo;
 	var nacionalidad;
+	var elejirApartado = parseInt(elejirApartado);
+	var validarSwitch = true;
+	var cont = 1;
 
+	validarSwitch = confirm("desea ingresar datos?");//para entrar al bucle
+	while(validarSwitch == true)                     // bucle para que ingrese cuantas veces quieran
+	{
+		if(cont == 1)
+		{
+			elejirApartado = prompt("elija uno de los apartados(1. Edad 2. Sexo 3. EstadoCivil 4. Sueldo 5. Legajo 6. Nacionalidad)")
+		}
+		else
+		{
+			cont--;
+		}//if(cont == 1)
+		elejirApartado = parseInt(elejirApartado);
+		switch(elejirApartado)                       //dar a elejir que apartado llenar con las funciones abajo
+		{
+			case 1:
+				edad = ingresarEdad(edad);
+				document.getElementById("Edad").value = edad;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			case 2:
+				sex = ingresarSex(sex);
+				document.getElementById("Sexo").value = sex;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			case 3:
+				estadoCivil = ingresarEstadoCivil(estadoCivil);
+				document.getElementById("EstadoCivil").value = estadoCivil;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			case 4:
+				sueldo = ingresarSueldo(sueldo);
+				document.getElementById("Sueldo").value = sueldo;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			case 5:
+				legajo = ingresarLegajo(legajo);
+				document.getElementById("Legajo").value = legajo;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			case 6:
+				nacionalidad = ingresarNacionalidad(nacionalidad);
+				document.getElementById("Nacionalidad").value = nacionalidad;
+				validarSwitch = confirm("desea seguir ingresando mas apartados?");
+				break;
+			default:
+				elejirApartado = prompt("ingreso mal el apartado, ingrese denuevo");
+			cont++;
+
+		}//switch(elejirApartado)
+	}//while(validarSwitch == true) 
 	
+}//Fin funcion
+//IDs: Edad, Sexo, EstadoCivil, Sueldo, Legajo, Nacionalidad
+
+function ingresarEdad(edad)
+{
 	edad = prompt("ingrese edad(edades entre 18 y 90)");//empezamos a pedir los datos por la edad
 	edad = parseInt(edad);
 	while(edad<18 || edad>90)// validar edad
@@ -29,6 +86,12 @@ function ComenzarIngreso ()
 		edad = parseInt(edad);
 	}//while(edad<18 || edad>90)
 
+	return edad;
+}//Fin funcion
+
+function ingresarSex(sex) 
+{
+	
 	sex = prompt("ingrese el sexo(f/m)");//pedir dato de sexo
 
 	while((sex!= "m" && sex!= "f") && (sex!= "M" && sex!= "F"))//validar el sexo
@@ -45,16 +108,24 @@ function ComenzarIngreso ()
 		sex = "femenino"; 
 	}//if(sex == "m" || sex == "M")
 
-	var cont = 1;// un pequeño contador que usare para saber si se equivoco en el estado civil
+	return sex;
+}
+
+function ingresarEstadoCivil(estadoCivil)
+{
+
+	var validarSwitch = true;
+	var cont = 1;
+	var switchEstado;
 	while(validarSwitch == true)// validar estado civil
 	{
 		if(cont == 1)
 		{
-			estadoCivil = prompt("igrese estado civil(1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos)");//pedir dato de estado civil
+			switchEstado = prompt("igrese estado civil(1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos)");//pedir dato de estado civil
 		}//if(cont == 1)
 
-		estadoCivil = parseInt(estadoCivil);
-		switch(estadoCivil) //asignar el estado civil en base a la consigna
+		switchEstado = parseInt(switchEstado);
+		switch(switchEstado) //asignar el estado civil en base a la consigna
 		{
 			case 1:
 				estadoCivil = "soltero";
@@ -73,11 +144,17 @@ function ComenzarIngreso ()
 				validarSwitch = false;
 				break;
 			default:
-				estadoCivil = prompt("igreso mal estado civil, ingrese de nuevo");
+				switchEstado = prompt("igreso mal estado civil, ingrese de nuevo");
 				cont++;
 				break;
 		}//switch(estadoCivil)
 	}//while(validarSwitch == true)
+
+	return estadoCivil;
+}//Fin funcion
+
+function ingresarSueldo(sueldo)
+{
 
 	sueldo = prompt("ingrese el sueldo(no menor a 8000)");//pedir dato de sueldo
 	sueldo = parseInt(sueldo);
@@ -87,6 +164,11 @@ function ComenzarIngreso ()
 		sueldo = parseInt(sueldo);
 	}//while(sueldo < 8000)
 
+	return sueldo;
+}//Fin funcion
+
+function ingresarLegajo(legajo)
+{
 	legajo = prompt("ingrese el legajo(no menor a 1000)");//pedir dato de legajo
 	legajo = parseInt(legajo);
 	while(legajo < 1000)
@@ -95,8 +177,14 @@ function ComenzarIngreso ()
 		legajo = parseInt(legajo);
 	}//while(sueldo < 1000)
 
+	return legajo;
+}//Fin funcion
+
+function ingresarNacionalidad(nacionalidad)
+{
+	var validarSwitch = true;
 	cont = 1;
-	while(validarSwitch == false)
+	while(validarSwitch == true)
 	{
 		if(cont == 1)
 		{
@@ -107,32 +195,23 @@ function ComenzarIngreso ()
 			case "a":
 			case "A":
 				nacionalidad = "argentinos";
-				validarSwitch = true;
+				validarSwitch = false;
 				break;
 			case "e":
 			case "E":
 				nacionalidad = "estranjero";
-				validarSwitch = true;
+				validarSwitch = false;
 				break;
 			case "n":
 			case "N":
 				nacionalidad = "nacionalizados";
-				validarSwitch = true;
+				validarSwitch = false;
 				break;
 			default:
 				nacionalidad = prompt("ingreso mal la nacionalidad, ingrese de nuevo la nacionalidad");
 				break;
+
 		}//switch(nacionalidad)
-    } //while(validarSwitch = false)
-
-//mostrar datos en pantalla
-	document.getElementById("Edad").value = edad;
-	document.getElementById("Sexo").value = sex;
-	document.getElementById("EstadoCivil").value = estadoCivil;
-	document.getElementById("Sueldo").value = sueldo;
-	document.getElementById("Legajo").value = legajo;
-	document.getElementById("Nacionalidad").value = nacionalidad;
- 
-}
-//IDs: Edad, Sexo, EstadoCivil, Sueldo, Legajo, Nacionalidad
-
+    } //while(validarSwitch = true)
+    return nacionalidad;
+}//Fin funcion
