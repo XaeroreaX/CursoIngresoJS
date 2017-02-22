69216eefb20cb4a291d548cb8b1480eccd8683c5
XaeroreaX <martinpitameglia@gmail.com>
2017-02-22 10:04:40 -0300

    while terminado
    document.write() + </br>

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -28,14 +28,24 @@ function Mostrar()
 	var promPositivo;
 	var promNegativo
 	var Diferencia;
+	var valida;
 	 
 	//confirmacion
 	var respuesta=true;
 
-	while(respuesta = true)
+	while(respuesta == true)
 	{
 		numero = prompt("ingresar un numero "+contador);
 		numero = parseInt(numero);
+		
+		valida = isNaN(numero);
+		while(valida==true)
+		{
+			alert("no es un un numero, ANIMAL!");
+			numero = prompt("ingresar un numero "+contador);
+			numero = parseInt(numero);
+			valida = isNaN(numero);
+		}
 		if(numero == 0)
 		{
 			cant0++;
@@ -55,18 +65,28 @@ function Mostrar()
 
 			if(numero%2 == 0)
 			{
-				cant0++
+				cant0++;
 			}
 		}
 
-
-
+		respuesta = confirm("quiere seguir ingresando?!!!");
 		contador++;
 	}//while(respuesta = true)
 
 	//PROMEDIAR
 	Diferencia = acumPositivo + acumNegativo;
 	promPositivo = acumPositivo/cantPositivos;
-
+	promNegativo = acumNegativo/cantNegativos;
+	document.write("La suma de los positivos: "+acumPositivo+"</br>la suma de las negativos: "+acumNegativo+"</br> cantidad de positivos:"+cantPositivos+"</br> cantidad de negativos:"+cantNegativos+"</br> cantidad de ceros:"+cant0+"</br> cantidad de numeros pares:"+cantPares+"</br> cantidad de numeros pares:"+cantPares+"</br> promedio de positivos:"+promPositivo+"</br> promedio de negativos:"+promNegativo+"</br>Diferencia entre positivos y negativos"+Diferencia);
 
 }//FIN DE LA FUNCIÓN
+/*Al presionar el botón pedir números hasta que el usuario quiera, mostar:
+1-Suma de los negativos.
+2-Suma de los positivos.
+3-Cantidad de positivos.
+4-Cantidad de negativos.
+5-Cantidad de ceros.
+6-Cantidad de números pares.
+7-Promedio de positivos. 
+8-Promedios de negativos. 
+9-Diferencia entre positivos y negativos, (positvos-negativos).*/
